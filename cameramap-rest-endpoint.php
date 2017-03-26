6d7e237fd338655ce48ffa9438bc93d55c3885f8
Fran Rodríguez <fran.rodriguez@logtrust.com>
2017-03-26 21:30:48 +0200

    Remove tap for vim spaces RETAB

--- cameramap-rest-endpoint.php
+++ cameramap-rest-endpoint.php
@@ -17,13 +17,13 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 add_action('rest_api_init', 'cameramap_register_routes');
 
 function cameramap_register_routes() {
-	register_rest_route( 'cameramap/v1', '/list', array(
-		'methods' => WP_REST_Server::READABLE,
-		'callback' => 'cameramap_get_around_items',
-	));
+    register_rest_route( 'cameramap/v1', '/list', array(
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => 'cameramap_get_around_items',
+    ));
 }
 
 function cameramap_get_around_items( WP_REST_Request $request ){
-	$data = $request->get_json_params();
-	return new WP_REST_Response( $data );
+    $data = $request->get_json_params();
+    return new WP_REST_Response( $data );
 }
