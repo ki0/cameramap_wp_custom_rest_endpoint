07ebf0b31d1cb2a63262923b4b646a54c20ea4f8
Fran Rodriguez <fran.rodriguez@logtrust.com>
2017-04-19 21:25:52 +0200

    Getting items

--- cameramap-rest-endpoint.php
+++ cameramap-rest-endpoint.php
@@ -18,12 +18,21 @@ add_action('rest_api_init', 'cameramap_register_routes');
 
 function cameramap_register_routes() {
     register_rest_route( 'cameramap/v1', '/list', array(
-        'methods' => WP_REST_Server::READABLE,
-        'callback' => 'cameramap_get_around_items',
+       'methods' => 'GET',
+       'callback' => 'cameramap_get_around_items',
     ));
 }
 
-function cameramap_get_around_items( WP_REST_Request $request ){
-    $data = $request->get_json_params();
-    return new WP_REST_Response( $data );
+function cameramap_get_around_items( $request ){
+    $radius_query = new WP_Query( array(
+       'posts_per_page' => -1,
+       'post_status' => 'publish',
+       'geo_mashup_query' => array(
+          'near_lat' => $request->get_param('lat'),
+          'near_lng' => $request->get_param('lng'),
+          'radius_km' => 2,
+       ),
+    ) );
+
+    return new WP_REST_Response( $radius_query->query() );
 }
